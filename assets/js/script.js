7ff63161a6cabb09f0210325082b18e30cedbd1c
Aaron Deas <deas.aaron@gmail.com>
2021-05-26 02:00:59 -0400

    Spent a long time working out how to show just the data for individual days and not times of a day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 var citiesCol = document.getElementById("city__col");
 const apiKey = "8e615c825fd43e5fdbc1ce461e5a0a9d";
+var testVar = "";
+var indexArray = [];
 var forecast;
 var now = moment();
+var date =
+	now._d.getFullYear() +
+	"-" +
+	`${now._d.getMonth() + 1}` +
+	"-" +
+	now._d.getDay();
 var weatherDisplay = document.getElementById("weather__display");
 var forecastCards = document.getElementById("forecast__cards");
 searchButton = document.getElementById("search__btn");
@@ -11,6 +19,7 @@ searchButton.addEventListener("click", function (e) {
 	Button tags use submit behavior by default. Prevent default needed to keep button from reloading by default 
 	*/
 	e.preventDefault();
+	indexArray = [];
 	var searchTerm = searchField.value;
 	console.log("Search button connected term:", searchTerm);
 	for (var i = 0; i < forecastCards.children.length; i++) {
@@ -18,10 +27,12 @@ searchButton.addEventListener("click", function (e) {
 	}
 	callApi(searchTerm);
 });
+
 function init() {
 	for (var i = 0; i < citiesCol.children.length; i++) {
 		citiesCol.children[i].addEventListener("click", function (e) {
 			callApi(e.target.id);
+			indexArray = [];
 			for (var i = 0; i < forecastCards.children.length; i++) {
 				forecastCards.children[i].children[0].children[1].innerHTML = "";
 			}
@@ -52,7 +63,14 @@ function callApi(value) {
 			return res.json();
 		})
 		.then(function (fdforecast) {
-			console.log(fdforecast);
+			for (var i = 0; i < fdforecast.list.length; i++) {
+				var listDates = fdforecast.list[i].dt_txt;
+				listDates = listDates.split(" ");
+				// console.log(listDates);
+				if (listDates[1] === "12:00:00") {
+					indexArray.push(i);
+				}
+			}
 			fiveDay(fdforecast);
 		});
 }
@@ -65,30 +83,32 @@ function displayForecast(value) {
 	weatherDisplay.children[4].innerText = "UV Index: " + value.main.humidity;
 }
 function fiveDay(fivedaylist) {
+	testVar = fivedaylist;
+
 	// console.log("Five Day Forecast", fiveday);
 	for (var i = 0; i < forecastCards.children.length; i++) {
-		forecastCards.children[i].children[0].children[0].innerText = `${
-			now._d.getMonth() + 1
-		}/${now._d.getDate() + i}/${now._d.getFullYear()}`;
-		// get Icon code
-		var iconCode = fivedaylist.list[i].weather[0].icon;
-		console.log(iconCode);
+		// indexArray = indexArray[i];
+		// console.log(indexArray);
+		var iconCode = fivedaylist.list[indexArray[i]].weather[0].icon;
+		console.log(fivedaylist.list[indexArray[i]].dt_txt);
 		var icon = document.createElement("img");
-		console.log(icon);
 		icon.setAttribute(
 			"src",
 			"http://openweathermap.org/img/wn/" + iconCode + "@2x.png"
 		);
-
+		var formattedDate = fivedaylist.list[indexArray[i]].dt_txt;
+		formattedDate = formattedDate.split(" ");
+		forecastCards.children[i].children[0].children[0].innerText =
+			formattedDate[0];
 		forecastCards.children[i].children[0].children[1].appendChild(icon);
 		forecastCards.children[i].children[0].children[2].innerText =
-			"Temp: " + fivedaylist.list[i].main.temp + " F";
+			"Temp: " + fivedaylist.list[indexArray[i]].main.temp + " F";
 		forecastCards.children[i].children[0].children[3].innerText =
-			"Wind: " + fivedaylist.list[i].wind.speed;
+			"Wind: " + fivedaylist.list[indexArray[i]].wind.speed;
 		forecastCards.children[i].children[0].children[4].innerText =
-			"Humidity: " + fivedaylist.list[i].main.humidity + "%";
+			"Humidity: " + fivedaylist.list[indexArray[i]].main.humidity + "%";
 		forecastCards.children[i].children[0].children[5].innerText =
-			"UV Index: " + fivedaylist.list[i].main.humidity + "%";
+			"UV Index: " + fivedaylist.list[indexArray[i]].main.humidity + "%";
 	}
 }
 init();
